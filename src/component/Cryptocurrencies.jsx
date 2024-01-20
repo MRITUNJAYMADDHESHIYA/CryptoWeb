3f0fbe3a9f0b24ed7190371e7eaca8b2cb33daca
Mritunjay <mritunjay112002@gmail.com>
2024-01-20 23:44:26 +0530

    cryptocurrency page

--- src/component/Cryptocurrencies.jsx
+++ src/component/Cryptocurrencies.jsx
@@ -49,7 +49,7 @@ const Cryptocurrencies = ({ simplified }) => {
                 extra={<img className="crypto-image" src={currency.iconUrl} />}
                 hoverable
               >
-                <p>Price: {millify(currency.price)}</p>
+                <p>Prices: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
                 <p>Daily Change: {currency.change}%</p>
               </Card>
