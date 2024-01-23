fc021d55a3d921818f3c196dca13d4a7ade8b06a
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2024-01-23 21:08:08 +0530

    Some new change in cryptocurrencies page

--- src/component/Cryptocurrencies.jsx
+++ src/component/Cryptocurrencies.jsx
@@ -14,9 +14,7 @@ const Cryptocurrencies = ({ simplified }) => {
 
   useEffect(() => {
     setCryptos(cryptosList?.data?.coins);
-
     const filteredData = cryptosList?.data?.coins.filter((item) => item.name.toLowerCase().includes(searchTerm));
-
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
 
@@ -24,31 +22,20 @@ const Cryptocurrencies = ({ simplified }) => {
 
   return (
     <>
+      {/* Here this div is for serching the crypto */}
       {!simplified && (
         <div className="search-crypto">
-          <Input
-            placeholder="Search Cryptocurrency"
-            onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
-          />
+          <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value.toLowerCase())} />
         </div>
       )}
+
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
-          <Col
-            xs={24}
-            sm={12}
-            lg={6}
-            className="crypto-card"
-            key={currency.uuid}
-          >
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
 
             {/* Note: Change currency.id to currency.uuid  */}
             <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
-              <Card
-                title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} />}
-                hoverable
-              >
+              <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} alt={`${currency.name} icon`}/>} hoverable>
                 <p>Prices: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
                 <p>Daily Change: {currency.change}%</p>
